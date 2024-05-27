17586a0e074b54ca982414fd0e09954bb18e4d33
Danil <eliseev.danii@yandex.ru>
2024-05-27 13:59:22 +0300

    add lesson5

---
+++ Task_1/Program.cs
@@ -0,0 +1,16 @@
+﻿// Вычислить факториал от натурального числа N
+
+int Fact (int number)
+{
+    if (number == 1 || number == 0)
+    {
+        Console.WriteLine($"Stop {number}");
+        return 1;
+    }
+    Console.WriteLine(number);
+    return number * Fact(number -1);
+}
+Console.WriteLine(Fact (5));
+
+// F11 - шаг заходом
+// F10 - шаг с обходом
